6d9765a4f377824fb6d4dcc0349e59f708f7b1ea
Andrew Wong <andrew.j.wong@outlook.com>
2021-09-29 00:37:13 +1000

    75% rate hm

--- project/lwe.m
+++ project/lwe.m
@@ -7,12 +7,12 @@ function untitled()
     n_tests = 10000;
     count = 0;
 
-    testVal = 1;
+    test_M = 1;
     for i = 1:n_tests
-      count = count + (runTest(testVal) == testVal);
+      count = count + (runTest(test_M) == test_M);
     end
 
-    fprintf("Statistics: %u / %u (%u%%)", count, n_tests, count / n_tests * 100)
+    fprintf("Statistics: %u / %u (%.2f%%)", count, n_tests, count / n_tests * 100)
 end
 
 function m = runTest(M)
@@ -28,8 +28,6 @@ function m = runTest(M)
 
   % bob generates a private key
   S = generatePrivateKey(m);
-  % S = [4; 7; 5; 5];
-
 
   [A, B] = generatePublicKey(S, m, n);
 
@@ -50,22 +48,22 @@ function [A, B] = generatePublicKey(S, m, n)
 
   A = randi(q, [n, m]); % uniform distribution
 
-  % TODO: what standard deviation goes here?
   % sqrt(n) <= std_dev << q
   stdDev = sqrt(n) + randi(fix(0.1 * q));
   e = fix(normrnd(0, stdDev, [n, 1]));
-
-  % B = mod(A*S, q); % no error
-  B = mod(A*S + e, q);
+  % e = round(randn(n, 1));
+  
+  B = mod(A*S, q); % no error
+  % B = mod(A*S + e, q);
 end
 
 function [u, v] = encryptBit(M, A, B)
   global q;
 
-  % sampleSize = n / 4
+  % sampleSize ~= n / 4
   sampleSize = fix(numel(B) / 4);
 
-  u = mod(sum(A(randsample(1:length(A), sampleSize),:)), q); %%%%%%%%%% Population must be a vector
+  u = mod(sum(A(randsample(1:length(A), sampleSize),:)), q);
   v = mod(sum(B(randsample(1:length(B), sampleSize),:)) - M * fix(q/2), q);
 end
 
@@ -73,20 +71,10 @@ function M = decryptBit(u, v, S)
   global q;
 
   D = mod(v - dot(u, S), q);
+
   % M = -fix(q / 4) <= D | D <= fix(q / 4);
-  
-  
-  % fprintf("Decoded D: %u\n", D);
-  % fprintf("q/2 is %u\n", q/2);
 
-
-  % M = abs(D) > fix(q / 4);
+  % M =  D > q/4 | D < -q/4;
+  M = abs(D) > fix(q / 4);  % 75%
   
-  % M = abs(D - q/2) <= q/4;
-
-  if D > q/4 && D < 3*q/4
-      M = 1;
-  else
-      M = 0;
-  end
 end
