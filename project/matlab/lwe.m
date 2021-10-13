aeb273b097f2d1d973c913e9fb72c24cc24bc58b
Andrew Wong <andrew.j.wong@outlook.com>
2021-10-14 01:42:25 +1100

    Move functions into a separate file

--- project/matlab/lwe.m
+++ project/matlab/lwe.m
@@ -1,45 +1,6 @@
-% function [y1,...,yN] = myfun(x1,...,xM)
-% -- function name: myfun
-% -- function args: x1, ..., xM
-% -- function retn: y1, ..., yN
+classdef lwe; methods(Static)
 
-function untitled()
-    n_tests = 10000;
-    count_0 = 0;
-    count_1 = 0;
-
-    for i = 1:n_tests
-      count_0 = count_0 + (runTest(0) == 0);
-    end
-
-    for i = 1:n_tests
-      count_1 = count_1 + (runTest(1) == 1);
-    end
-
-    fprintf("Statistics (%u tests) | M=0 %.2f%% | M=1 %.2f%%", n_tests, count_0 / n_tests * 100, count_1 / n_tests * 100)
-
-end
-
-function m = runTest(M)
-
-  global q;
-
-  %%%%%%% SHARED
-  q = 23;
-  %%%%%%% SHARED
-
-  m = 4;
-  n = 12;
-
-  S = generatePrivateKey(m);
-
-  [A, B] = generatePublicKey(S, m, n);
-
-  [u, v] = encryptBit(M, A, B);
-  m = decryptBit(u, v, S);
-end
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function S = generatePrivateKey(m)
     global q;
 
@@ -84,4 +45,5 @@ function M = decryptBit(u, v, S)
   M = D > q/4 & D < 3*q/4; 
 
 end
- 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end end
